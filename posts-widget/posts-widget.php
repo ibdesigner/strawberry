564a3c7b66679c12b40da0ff428ee5419e0bdcf9
bogdan.iacob <bogdan.iacob@D-RO6695-09469.RINDOM.RINGIER.RO>
2014-08-12 17:47:42 +0300

    generation time

--- posts-widget/posts-widget.php
+++ posts-widget/posts-widget.php
@@ -6,24 +6,30 @@ if (!class_exists('Strawberry')) {
 class Strawberry_posts_widget extends WP_Widget {
 
     private $template_dir;
+    private $widget_key;
 
-    public function __construct() {
+    public function __construct($id_base = false) {
 
         $this->template_dir = dirname(__FILE__) . "/templates/";
 
         parent::__construct(
                 'strawberry_posts_wigdet', __('Taxonomy Posts Widget', 'strawberry'), array('description' => __('Listing posts from selected Taxonomies', 'text_domain'),)
         );
+        
+        $this->widget_key = "widget-" . $this->id;
+        
+        if($this->updated === true){
+            delete_transient($this->widget_key);
+        }
     }
 
     public function widget($args, $instance) {
         if(defined('DEBUG_WIDGETS') && DEBUG_WIDGETS === true){
             $time = explode(' ', microtime());
             $start = $time[1] + $time[0];
-        }
-        $widget_key = "widget-" . str_replace(' ', '-', strtolower($instance['title']));
+        }                      
     
-        $strawberry_widget_cache = get_transient($widget_key);
+        $strawberry_widget_cache = get_transient($this->widget_key);
 
         if (false === $strawberry_widget_cache) {
             if (!isset($instance['cache_time']) || $instance['cache_time'] == "") {
@@ -78,10 +84,10 @@ class Strawberry_posts_widget extends WP_Widget {
 
             $output .= $args['after_widget'];                       
 
-            set_transient($widget_key, $output, $instance['cache_time']);
+            set_transient($this->widget_key, $output, $instance['cache_time']);
         }
         
-        echo get_transient($widget_key);
+        echo get_transient($this->widget_key);
         if(defined('DEBUG_WIDGETS') && DEBUG_WIDGETS === true){
             $time = explode(' ', microtime());
             $finish  = $time[1] + $time[0];
